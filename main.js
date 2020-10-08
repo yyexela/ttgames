6705ca98ada8abe18bf1a355291aac7421d40d32
yyexela <yyexela@disroot.org>
2020-10-08 12:06:57 -0600

    Updated website to include lobby-connection

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // IP address of AWS server
 const IPADDR = '3.130.99.109';
 const PORT = '443';
+var lobbyID = "";
+var username = "";
 
 function display_help(){
     document.getElementById("help-overlay").style.display = "flex";
@@ -13,8 +15,8 @@ function hide_help(){
 function submit_button(){
     // Get data from form
     let name = document.getElementById("username-input").value;
-    let code = document.getElementById("game-code-input").value;
-    console.log("Username: \"" + name + "\", code: \"" + code +"\"");
+    let gamecode = document.getElementById("game-code-input").value;
+    console.log("Username: \"" + name + "\", game code: \"" + gamecode +"\"");
 
     // Establish websocket connection
     var aWebSocket = new WebSocket('wss://' + IPADDR + ':' + PORT);
@@ -22,20 +24,46 @@ function submit_button(){
     // Install event handlers
     aWebSocket.onclose = function(event) {
         console.log("WebSocket is closed");
+
+        // Check to see if we have already joined a lobby
+        if(document.getElementById("login-page").style.display == "none"){
+            document.getElementById('lobby-page').style.display = "none";
+            document.getElementById('login-page').style.display = "grid";
+            document.getElementById('error-message').innerHTML = "Error: Connection closed";
+            document.getElementById('lobby-players').innerHTML = "";
+            lobbyID = "";
+        }
     };
 
     aWebSocket.onopen = function(event) {
         console.log("Connected to server");
-        aWebSocket.send(name + "," + code);
+        aWebSocket.send(name + "," + gamecode);
     };
 
     aWebSocket.onmessage = function(event) {
-        console.debug("WebSocket message received: ", event);
+        console.log("WebSocket message received: ", event);
         console.log("Message: " + event.data);
+        let statuscode = event.data.substring(0, event.data.indexOf(','));
+        if(statuscode == "1"){
+            document.getElementById('error-message').innerHTML = "Error: Invalid Game ID";
+        } else if (statuscode == "2"){
+            document.getElementById('error-message').innerHTML = "Error: Invalid data format (dev only)";
+        } else if (statuscode == "3"){
+            document.getElementById('error-message').innerHTML = "Error: Empty field(s)";
+        } else if (statuscode != "0"){
+            document.getElementById('error-message').innerHTML = "Error: Internal server error";
+        } else {
+            lobbyID = gamecode;
+            username = name;
+            document.getElementById('lobby-page').style.display = "grid";
+            document.getElementById('login-page').style.display = "none";
+            document.getElementById('error-message').innerHTML = "";
+            document.getElementById('lobby-id').innerHTML = lobbyID;
+
+            let players = document.getElementById('lobby-players');
+            players.innerHTML += "<div class=\"lobby-player\" id=\"" + username + "\">" + username +"</div>"
+        }
     };
 
-
-    // 3 second timer before closing connection
-    setTimeout(function(){ aWebSocket.close(1000, "Successfully closed communication"); }, 3000);
 }
 
